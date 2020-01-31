c4d5dd86d7a222b1aa219530e084bdd8eede9d44
qer <qilongyu@126.com>
2020-01-31 21:45:37 +0800

    输出到txt版本

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-	
+let fs = require('fs');
+let options = {
+  flags: 'a',     // append模式
+  encoding: 'utf8',  // utf8编码
+};
+let stdout = fs.createWriteStream('./mynotes.txt', options);
+// 创建logger
+let logger = new console.Console(stdout);
+
+// 时间戳转换
 function getLocalTime(nS) {     
    return new Date(parseInt(nS)*1).toLocaleString().replace(/:\d{1,2}$/,' ');     
 }
@@ -6,10 +15,10 @@ function getLocalTime(nS) {
 var jsonObj = require('./mi_notes.json'); 
 // console.log(jsonObj); 
 for(var i in jsonObj) {
-	console.log("------------------------------------------------");
-	console.log("序号:"+i);
-	console.log("\n创建时间:"+getLocalTime(jsonObj[i].createDate)+"\n修改时间:"+getLocalTime(jsonObj[i].modifyDate));
-	console.log("\n内容---Start:");
-	console.log(jsonObj[i].snippet);
-	console.log("内容---End\n");
+	logger.log("------------------------------------------------");
+	logger.log("序号:"+i);
+	logger.log("\n创建时间:"+getLocalTime(jsonObj[i].createDate)+"\n修改时间:"+getLocalTime(jsonObj[i].modifyDate));
+	logger.log("\n内容---Start:\n**");
+	logger.log(jsonObj[i].snippet);
+	logger.log("**\n内容---End\n");
 }
