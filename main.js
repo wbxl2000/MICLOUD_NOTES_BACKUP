405a5bb8c4219cd15c9e82cc8732e8ccdaa8c484
qer <qilongyu@126.com>
2020-01-31 21:44:28 +0800

    无文件操作

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+	
 function getLocalTime(nS) {     
    return new Date(parseInt(nS)*1).toLocaleString().replace(/:\d{1,2}$/,' ');     
 }
-console.log(getLocalTime(1580449869755));
+
+var jsonObj = require('./mi_notes.json'); 
+// console.log(jsonObj); 
+for(var i in jsonObj) {
+	console.log("------------------------------------------------");
+	console.log("序号:"+i);
+	console.log("\n创建时间:"+getLocalTime(jsonObj[i].createDate)+"\n修改时间:"+getLocalTime(jsonObj[i].modifyDate));
+	console.log("\n内容---Start:");
+	console.log(jsonObj[i].snippet);
+	console.log("内容---End\n");
+}
